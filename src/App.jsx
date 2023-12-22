27569dfa41a0f64841a948d4980b3dfcff59ec5f
Irvan <darknet1255@gmail.com>
2023-12-22 19:56:50 +0700

    Create Square Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+import Square from './components/Square'
 
 const App = () => {
   return (
-    <>
-      
-    </>
+    <div className='board'>
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+    </div>
   )
 }
 
---
+++ src/components/Square.jsx
@@ -0,0 +1,5 @@
+const Square = () => {
+  return <button className="square"></button>;
+};
+
+export default Square;

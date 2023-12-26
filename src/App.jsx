185014d623ddd2e935de44a29638418fbb4e0227
Irvan <darknet1255@gmail.com>
2023-12-26 17:33:33 +0700

    Membuat state untuk menentukan apakah giliran x yang jalan
    X jalan terlebih dahulu karna default valuenya true

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Square from "./components/Square";
 
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
 
   return (
     <div className="board">

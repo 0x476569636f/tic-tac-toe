fac830aade3507069468a2c08b7950ff381c0403
Irvan <darknet1255@gmail.com>
2023-12-26 18:25:43 +0700

    Membuat array yang mereprentasikan pemenang

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,19 @@ const App = () => {
     setXIsNext(!xIsNext);
   };
 
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+  };
+
   return (
     <div className="board">
       <Square squareValue={squares[0]} onSquareClick={() => handleClick(0)} />

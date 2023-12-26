f1e4c60ab993bfd11d7bc6c5a0fcf7eac8c3e2c9
Irvan <darknet1255@gmail.com>
2023-12-26 17:39:47 +0700

    Merepresentasikan nilai squares

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,15 @@ const App = () => {
 
   return (
     <div className="board">
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
+      <Square squareValue={squares[0]} />
+      <Square squareValue={squares[1]} />
+      <Square squareValue={squares[2]} />
+      <Square squareValue={squares[3]} />
+      <Square squareValue={squares[4]} />
+      <Square squareValue={squares[5]} />
+      <Square squareValue={squares[6]} />
+      <Square squareValue={squares[7]} />
+      <Square squareValue={squares[8]} />
     </div>
   );
 };
